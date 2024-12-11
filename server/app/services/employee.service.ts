0ce4ccd40c740316fc5aa5f041fb80d8ef47babe
Anirban Karmakar <anirbankarmakar0190@gmail.com>
2024-12-11 18:26:27 +0530

    collate hierarchy service

--- server/app/services/employee.service.ts
+++ server/app/services/employee.service.ts
@@ -27,125 +27,121 @@ export async function findEmployeeById(id: EmployeeDocument["_id"]) {
 export async function collateEmployeeTree() {
   try {
     return await employeeModel.aggregate([
-        {
-          $addFields: {
-            expanded: true,
-            type: "person",
+      {
+        $match: { designation: "CEO" },
+      },
+      {
+        $graphLookup: {
+          from: "employees",
+          startWith: "$_id",
+          connectFromField: "_id",
+          connectToField: "reporting",
+          as: "allChildren",
+        },
+      },
+      {
+        $addFields: {
+          children: {
+            $filter: {
+              input: "$allChildren",
+              as: "child",
+              cond: { $eq: ["$$child.reporting", "$_id"] },
+            },
           },
         },
-        // {
-        //   $lookup: {
-        //     from: "employees",
-        //     localField: "_id",
-        //     foreignField: "reporting",
-        //     as: "children",
-        //   },
-        // },
-        // {
-        //   $addFields: {
-        //     children: {
-        //       expanded: true,
-        //       type: "person",
-        //     },
-        //   },
-        // },
-        {
-            $project: {
-                __v: 0
-            }
-        }
-      //   {
-      //     $merge:
-      //   },
-      //   {
-      //     $unwind: {
-      //       path: "$children",
-      //       // preserveNullAndEmptyArrays: true
-      //     },
-      //   },
-      //   {
-      //     $group: {
-      //       _id: {
-      //         _id: "$_id",
-      //         fullName: "$fullName",
-      //         designation: "$designation",
-      //         date_of_birth: "$date_of_birth",
-      //         experience_years: "$experience_years",
-      //         picture: "$picture",
-      //         createdAt: "$createdAt",
-      //         updatedAt: "$updatedAt",
-      //         __v: "$__v",
-      //         children: "$children"
-      //       },
-      //       // children: { $push: "$children" },
-      //     },
-      //   },
-      //   {
-      //     $addFields: {
-      //       expanded: true,
-      //       type: "person",
-      //     },
-      //   },
-      ]);
-    
+      },
+      {
+        $project: {
+          allChildren: 0, // Remove the flattened list
+        },
+      },
+      {
+        $set: {
+          children: {
+            $map: {
+              input: "$children",
+              as: "child",
+              in: {
+                $mergeObjects: [
+                  "$$child",
+                  {
+                    children: {
+                      $arrayElemAt: [
+                        employeeModel.aggregate([
+                          {
+                            $match: {
+                              reporting: "$$child._id",
+                            },
+                          },
+                        ]),
+                      ],
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        },
+      },
+    ]);
+
     // .aggregate([
-    //   {
-    //     $match: { designation: "CEO" }, // Start from the root node (CEO)
-    //   },
-    //   {
-    //     $graphLookup: {
-    //       from: "employees", // The collection name
-    //       startWith: "$_id", // Start with the CEO's `_id`
-    //       connectFromField: "_id", // Match the current document's `_id`
-    //       connectToField: "reporting", // Match the `reporting` field in the collection
-    //       as: "allChildren", // Flattened list of all descendants
+    //     {
+    //       $addFields: {
+    //         expanded: true,
+    //         type: "person",
+    //       },
     //     },
-    //   },
-    //   {
-    //     $addFields: {
-    //       children: {
-    //         $filter: {
-    //           input: "$allChildren",
-    //           as: "child",
-    //           cond: { $eq: ["$$child.reporting", "$_id"] }, // Immediate children only
+    //     {
+    //       $lookup: {
+    //         from: "employees",
+    //         localField: "_id",
+    //         foreignField: "reporting",
+    //         as: "children",
+    //       },
+    //     },
+    //     {
+    //       $addFields: {
+    //         children: {
+    //           expanded: true,
+    //           type: "person",
     //         },
     //       },
     //     },
-    //   },
-    //   {
-    //     $project: {
-    //       allChildren: 0, // Remove the flattened list
+    //     {
+    //         $project: {
+    //             __v: 0
+    //         }
     //     },
-    //   },
-    //   {
-    //     $set: {
-    //       children: {
-    //         $map: {
-    //           input: "$children",
-    //           as: "child",
-    //           in: {
-    //             $mergeObjects: [
-    //               "$$child",
-    //               {
-    //                 children: {
-    //                   $arrayElemAt: [
-    //                     employeeModel.aggregate([
-    //                       {
-    //                         $match: {
-    //                           reporting: "$$child._id",
-    //                         },
-    //                       },
-    //                     ]),
-    //                   ],
-    //                 },
-    //               },
-    //             ],
-    //           },
-    //         },
+    //     {
+    //       $unwind: {
+    //         path: "$children",
+    //         // preserveNullAndEmptyArrays: true
     //       },
     //     },
-    //   },
-    // ]);
+    //     {
+    //       $group: {
+    //         _id: {
+    //           _id: "$_id",
+    //           fullName: "$fullName",
+    //           designation: "$designation",
+    //           date_of_birth: "$date_of_birth",
+    //           experience_years: "$experience_years",
+    //           picture: "$picture",
+    //           createdAt: "$createdAt",
+    //           updatedAt: "$updatedAt",
+    //           __v: "$__v",
+    //         },
+    //         children: { $push: "$children" },
+    //       },
+    //     },
+    //     {
+    //       $addFields: {
+    //         expanded: true,
+    //         type: "person",
+    //       },
+    //     },
+    //   ]);
   } catch (error: any) {
     throw new Error(error);
   }
